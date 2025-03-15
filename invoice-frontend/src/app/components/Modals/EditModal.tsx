b43d5f73a040815f1ed6ccf46a8250a4b3d3aa86
G3nK3n <ken_serrano@Hotmail.com>
2025-03-14 23:54:49 -0400

    EditModal Fixes

--- invoice-frontend/src/app/components/Modals/EditModal.tsx
+++ invoice-frontend/src/app/components/Modals/EditModal.tsx
@@ -50,19 +50,21 @@ export default function EditModal({ theInvoiceDetail, onClose }: InvoiceInfoDeta
         <Box sx={{ display: 'block', position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', background: '#00000099', zIndex: '1' }}>
 
             {/* This is the actual box for the form */}
-            <Box sx={{ padding: '55px', position: 'fixed', background: 'white', width: '719px', height: '512px', top: '256px', left: '463px', transform: 'translate(-50%, -50%)', display: 'flex', flexDirection: 'column', borderRadius: '0px 20px 20px 0px', zIndex: '1' }}>
+            <Box sx={{ padding: '55px', position: 'fixed', background: 'white', maxHeight: '100vh', width: '719px', left: '103px', display: 'flex', flexDirection: 'column', borderRadius: '0px 20px 20px 0px', zIndex: '1', overflow: 'scroll' }}>
 
+                {/* This is the Invoice Title */}
                 <Box>
                     <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '18px', color: "black", fontWeight: '400', display: 'inline-block' }}>
                         <b>Edit <span style={{ color: 'grey' }}>#</span>{theInvoiceDetail?.InvoiceID}</b>
                     </Typography>
                 </Box>
 
-                <Box>
+                {/* This is the Bills From section */}
+                <Box sx={{ mt: '50px' }}>
                     <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '16px', color: "#7C5DFA", fontWeight: '400', display: 'block' }}><b>Bill From</b></Typography>
-                    <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>Street Address</Typography>
+                    <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block', mt: '20px' }}>Street Address</Typography>
                     <TextField
-                        defaultValue={theInvoiceDetail?.ClientAddress}
+                        defaultValue={theInvoiceDetail?.BillsFromAddress}
                         id="outlined-basic"
                         label={false}
                         variant="outlined"
@@ -75,13 +77,33 @@ export default function EditModal({ theInvoiceDetail, onClose }: InvoiceInfoDeta
                                 }
                             }
                         }}
+                        sx={{ mt: '5px' }}
                     />
-                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: '20px' }}>
                         <Box>
                             <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>City</Typography>
                             <TextField
-                                defaultValue={theInvoiceDetail?.ClientCity}
-                                sx={{ width: '180px' }}
+                                defaultValue={theInvoiceDetail?.BillsFromCity}
+                                sx={{ width: '180px', mt: '5px' }}
+                                id="outlined-basic"
+                                label={false}
+                                variant="outlined"
+                                size='small'
+                                slotProps={{
+                                    input: {
+                                        style: {
+                                            fontWeight: 'bold'
+                                        }
+                                    }
+                                }}
+
+                            />
+                        </Box>
+                        <Box>
+                            <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>Post Code</Typography>
+                            <TextField
+                                defaultValue={theInvoiceDetail?.BillsFromPostalCode}
+                                sx={{ width: '180px', mt: '5px' }}
                                 id="outlined-basic"
                                 label={false}
                                 variant="outlined"
@@ -96,10 +118,108 @@ export default function EditModal({ theInvoiceDetail, onClose }: InvoiceInfoDeta
                             />
                         </Box>
                         <Box>
+                            <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>Country</Typography>
+                            <TextField
+                                defaultValue={theInvoiceDetail?.BillsFromCountry}
+                                sx={{ width: '180px', mt: '5px' }}
+                                id="outlined-basic"
+                                label={false}
+                                variant="outlined"
+                                size='small'
+                                slotProps={{
+                                    input: {
+                                        style: {
+                                            fontWeight: 'bold'
+                                        }
+                                    }
+                                }}
+                            />
+                        </Box>
+                    </Box>
+                </Box>
+
+                {/* This is the Bills To section */}
+                <Box>
+                    <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '16px', color: "#7C5DFA", fontWeight: '400', display: 'block', mt: '50px' }}><b>Bill To</b></Typography>
+                    <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block', mt: '20px' }}>Client's Name</Typography>
+                    <TextField
+                        defaultValue={theInvoiceDetail?.ClientName}
+                        id="outlined-basic"
+                        label={false}
+                        variant="outlined"
+                        size='small'
+                        fullWidth
+                        slotProps={{
+                            input: {
+                                style: {
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        }}
+                        sx={{ mt: '5px' }}
+                    />
+
+                    <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block', mt: '20px' }}>Client's Email</Typography>
+                    <TextField
+                        defaultValue={theInvoiceDetail?.ClientEmail}
+                        id="outlined-basic"
+                        label={false}
+                        variant="outlined"
+                        size='small'
+                        fullWidth
+                        slotProps={{
+                            input: {
+                                style: {
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        }}
+                        sx={{ mt: '5px' }}
+                    />
+
+                    <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block', mt: '20px' }}>Street Address</Typography>
+                    <TextField
+                        defaultValue={theInvoiceDetail?.ClientAddress}
+                        id="outlined-basic"
+                        label={false}
+                        variant="outlined"
+                        size='small'
+                        fullWidth
+                        slotProps={{
+                            input: {
+                                style: {
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        }}
+                        sx={{ mt: '5px' }}
+                    />
+
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: '20px' }}>
+                        <Box>
+                            <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>City</Typography>
+                            <TextField
+                                defaultValue={theInvoiceDetail?.ClientCity}
+                                sx={{ width: '180px', mt: '5px' }}
+                                id="outlined-basic"
+                                label={false}
+                                variant="outlined"
+                                size='small'
+                                slotProps={{
+                                    input: {
+                                        style: {
+                                            fontWeight: 'bold'
+                                        }
+                                    }
+                                }}
+
+                            />
+                        </Box>
+                        <Box>
                             <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>Post Code</Typography>
                             <TextField
                                 defaultValue={theInvoiceDetail?.ClientPostalCode}
-                                sx={{ width: '180px' }}
+                                sx={{ width: '180px', mt: '5px' }}
                                 id="outlined-basic"
                                 label={false}
                                 variant="outlined"
@@ -117,7 +237,7 @@ export default function EditModal({ theInvoiceDetail, onClose }: InvoiceInfoDeta
                             <Typography sx={{ fontFamily: leagueSpartan.style.fontFamily, fontSize: '14px', color: "#7E88C3", fontWeight: '400', display: 'block' }}>Country</Typography>
                             <TextField
                                 defaultValue={theInvoiceDetail?.ClientCountry}
-                                sx={{ width: '180px' }}
+                                sx={{ width: '180px', mt: '5px' }}
                                 id="outlined-basic"
                                 label={false}
                                 variant="outlined"
@@ -132,7 +252,10 @@ export default function EditModal({ theInvoiceDetail, onClose }: InvoiceInfoDeta
                             />
                         </Box>
                     </Box>
+
                 </Box>
+
+                {/* This is the Buttons From section */}
                 <Box>
                     <Button onClick={() => onClose()}>Close</Button>
                     <Button>Save Changes</Button>
